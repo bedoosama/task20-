15cb5d466d67c14e742728eda65d76501cb65201
bedoosama <b2153731@gmail.com>
2026-05-10 22:20:25 +0300

    Add files via upload

---
+++ project.js/js/main.js
@@ -0,0 +1,7 @@
+
+
+// document.getElementById('demo').innerHTML ="hello word from js code";
+
+// document.getElementById('demo').style.textAlign ="center";
+
+// document.getElementById('demo').style.backgroundColor="rgb(255,0,0)";
